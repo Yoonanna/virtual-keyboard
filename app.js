3c870b10eb59e9edce861b8ec4c877b223210a91
Yoonanna <Yanykovaanya@gmail.com>
2023-04-28 17:22:59 +0200

    feat: implement responsive of keys

--- app.js
+++ app.js
@@ -9,8 +9,9 @@ wrapper.append(caption);
 
 let textarea = document.createElement('textarea');
 textarea.className = 'textarea';
+textarea.autofocus = true;
 wrapper.append(textarea);
-textarea.setAttribute('rows', 10);
+textarea.setAttribute('rows', 15);
 
 
 let keyboard = document.createElement('div');
@@ -38,10 +39,6 @@ row5.className = 'keyboard__row5';
 row5.classList.add('row');
 keyboard.append(row5);
 
-// let key = document.createElement('button');
-// key.className = 'keyboard__key';
-// row.append(key);
-// key.innerText = 'a';
 
 let info = document.createElement('p');
 info.className = 'info';
@@ -49,49 +46,250 @@ wrapper.append(info);
 info.innerText = 'Клавиатура создана в ОС Windows';
 
 (function () {
-    const allKeys1 = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'];
-    allKeys1.forEach(el => {
+  const allKeys1 = [
+    { keyCode: 'Backquote', keyName: '`' },
+    { keyCode: 'Digit1', keyName: '1' },
+    { keyCode: 'Digit2', keyName: '2' },
+    { keyCode: 'Digit3', keyName: '3' },
+    { keyCode: 'Digit4', keyName: '4' },
+    { keyCode: 'Digit5', keyName: '5' },
+    { keyCode: 'Digit6', keyName: '6' },
+    { keyCode: 'Digit7', keyName: '7' },
+    { keyCode: 'Digit8', keyName: '8' },
+    { keyCode: 'Digit9', keyName: '9' },
+    { keyCode: 'Digit0', keyName: '0' },
+    { keyCode: 'Minus', keyName: '-' },
+    { keyCode: 'Equal', keyName: '=' },
+    { keyCode: 'Backspace', keyName: 'Backspace' },
+  ];
+  allKeys1.forEach(el => {
 
-        let key = document.createElement('div');
-        key.className = 'keyboard__key';
-        key.classList.add(`key${el}`);
-        row1.append(key);
-        key.innerText = el;
+    let key = document.createElement('div');
+    key.className = 'keyboard__key';
+    key.classList.add(`${el.keyCode}`);
+    row1.append(key);
+    key.innerText = el.keyName;
+
+    document.addEventListener('keydown', function keyDown(event) {
+      if (event.code === el.keyCode) {
+        key.classList.add('active');
+        textarea.value += `${el.keyName}`;
+
+      }
+    });
+    document.addEventListener('keyup', function keyUp(event) {
+      if (event.code === el.keyCode) {
+        key.classList.remove('active');
+      }
+    });
+
+    key.addEventListener('mousedown', () => {
+      key.classList.add('active');
+      textarea.value += `${el.keyName}`;
+
+    }
+    );
+    key.addEventListener('mouseup', () => {
+      key.classList.remove('active');
     }
     );
 
-    const allKeys2 = ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del'];
-    allKeys2.forEach(el => {
-        let key = document.createElement('div');
-        key.className = 'keyboard__key';
-        key.classList.add(`key${el}`);
-        row2.append(key);
-        key.innerText = el;
+  }
+  );
+
+
+  const allKeys2 = [
+    { keyCode: 'Tab', keyName: 'Tab' },
+    { keyCode: 'KeyQ', keyName: 'q' },
+    { keyCode: 'KeyW', keyName: 'w' },
+    { keyCode: 'KeyE', keyName: 'e' },
+    { keyCode: 'KeyR', keyName: 'r' },
+    { keyCode: 'KeyT', keyName: 't' },
+    { keyCode: 'KeyY', keyName: 'y' },
+    { keyCode: 'KeyU', keyName: 'u' },
+    { keyCode: 'KeyI', keyName: 'i' },
+    { keyCode: 'KeyO', keyName: 'o' },
+    { keyCode: 'KeyP', keyName: 'p' },
+    { keyCode: 'BracketLeft', keyName: '[' },
+    { keyCode: 'BracketRight', keyName: ']' },
+    { keyCode: 'Backslash', keyName: '\\' },
+    { keyCode: 'Delete', keyName: 'Del' },
+  ];
+  allKeys2.forEach(el => {
+
+    let key = document.createElement('div');
+    key.className = 'keyboard__key';
+    key.classList.add(`${el.keyCode}`);
+    row2.append(key);
+    key.innerText = el.keyName;
+
+    document.addEventListener('keydown', function keyDown(event) {
+      if (event.code === el.keyCode) {
+        key.classList.add('active');
+        textarea.value += `${el.keyName}`;
+
+      }
     });
-    const allKeys3 = ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Backslash', 'Enter'];
-    allKeys3.forEach(el => {
-        let key = document.createElement('div');
-        key.className = 'keyboard__key';
-        key.classList.add(`key${el}`);
-        row3.append(key);
-        key.innerText = el;
+    document.addEventListener('keyup', function keyUp(event) {
+      if (event.code === el.keyCode) {
+        key.classList.remove('active');
+      }
     });
 
-    const allKeys4 = ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '⮝', 'Shift'];
-    allKeys4.forEach(el => {
-        let key = document.createElement('div');
-        key.className = 'keyboard__key';
-        key.classList.add(`key${el}`);
-        row4.append(key);
-        key.innerText = el;
+    key.addEventListener('mousedown', () => {
+      key.classList.add('active');
+      textarea.value += `${el.keyName}`;
+
+    }
+    );
+    key.addEventListener('mouseup', () => {
+      key.classList.remove('active');
+    }
+    );
+  }
+  );
+
+
+  const allKeys3 = [
+    { keyCode: 'CapsLock', keyName: 'Caps' },
+    { keyCode: 'KeyA', keyName: 'a' },
+    { keyCode: 'KeyS', keyName: 's' },
+    { keyCode: 'KeyD', keyName: 'd' },
+    { keyCode: 'KeyF', keyName: 'f' },
+    { keyCode: 'KeyG', keyName: 'g' },
+    { keyCode: 'KeyH', keyName: 'h' },
+    { keyCode: 'KeyJ', keyName: 'j' },
+    { keyCode: 'KeyK', keyName: 'k' },
+    { keyCode: 'KeyL', keyName: 'l' },
+    { keyCode: 'Semicolon', keyName: ';' },
+    { keyCode: 'Quote', keyName: "'" },
+    { keyCode: 'Enter', keyName: 'Enter' },
+  ];
+  allKeys3.forEach(el => {
+
+    let key = document.createElement('div');
+    key.className = 'keyboard__key';
+    key.classList.add(`${el.keyCode}`);
+    row3.append(key);
+    key.innerText = el.keyName;
+
+    document.addEventListener('keydown', function keyDown(event) {
+      if (event.code === el.keyCode) {
+        key.classList.add('active');
+        textarea.value += `${el.keyName}`;
+
+      }
     });
-    const allKeys5 = ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '⮜', '⮟', '➤', 'Ctrl'];
-    allKeys5.forEach(el => {
-        let key = document.createElement('div');
-        key.className = 'keyboard__key';
-        key.classList.add(`key${el}`);
-        row5.append(key);
-        key.innerText = el;
+    document.addEventListener('keyup', function keyUp(event) {
+      if (event.code === el.keyCode) {
+        key.classList.remove('active');
+      }
     });
+
+    key.addEventListener('mousedown', () => {
+      key.classList.add('active');
+      textarea.value += `${el.keyName}`;
+
+    }
+    );
+    key.addEventListener('mouseup', () => {
+      key.classList.remove('active');
+    }
+    );
+  }
+  );
+
+  const allKeys4 = [
+    { keyCode: 'ShiftLeft', keyName: 'Shift' },
+    { keyCode: 'KeyZ', keyName: 'a' },
+    { keyCode: 'KeyX', keyName: 's' },
+    { keyCode: 'KeyC', keyName: 'd' },
+    { keyCode: 'KeyV', keyName: 'f' },
+    { keyCode: 'KeyB', keyName: 'g' },
+    { keyCode: 'KeyN', keyName: 'h' },
+    { keyCode: 'KeyN', keyName: 'j' },
+    { keyCode: 'KeyM', keyName: 'k' },
+    { keyCode: 'Comma', keyName: ',' },
+    { keyCode: 'Period', keyName: '.' },
+    { keyCode: 'Slash', keyName: '/' },
+    { keyCode: 'ArrowUp', keyName: '⮝' },
+    { keyCode: 'ShiftRight', keyName: 'Shift' },
+  ];
+  allKeys4.forEach(el => {
+
+    let key = document.createElement('div');
+    key.className = 'keyboard__key';
+    key.classList.add(`${el.keyCode}`);
+    row4.append(key);
+    key.innerText = el.keyName;
+    document.addEventListener('keydown', function keyDown(event) {
+      if (event.code === el.keyCode) {
+        key.classList.add('active');
+        textarea.value += `${el.keyName}`;
+
+      }
+    });
+    document.addEventListener('keyup', function keyUp(event) {
+      if (event.code === el.keyCode) {
+        key.classList.remove('active');
+      }
+    });
+
+    key.addEventListener('mousedown', () => {
+      key.classList.add('active');
+      textarea.value += `${el.keyName}`;
+
+    }
+    );
+    key.addEventListener('mouseup', () => {
+      key.classList.remove('active');
+    }
+    );
+  }
+  );
+
+  const allKeys5 = [
+    { keyCode: 'ControlLeft', keyName: 'Ctrl' },
+    { keyCode: 'MetaLeft', keyName: 'Win' },
+    { keyCode: 'AltLeft', keyName: 'Alt' },
+    { keyCode: 'Space', keyName: 'Space' },
+    { keyCode: 'AltRight', keyName: 'Alt' },
+    { keyCode: 'ArrowLeft', keyName: '⮜' },
+    { keyCode: 'ArrowDown', keyName: '⮟' },
+    { keyCode: 'ArrowRight', keyName: '➤' },
+    { keyCode: 'ControlRight', keyName: 'Ctrl' },
+  ];
+  allKeys5.forEach(el => {
+
+    let key = document.createElement('div');
+    key.className = 'keyboard__key';
+    key.classList.add(`${el.keyCode}`);
+    row5.append(key);
+    key.innerText = el.keyName;
+    document.addEventListener('keydown', function keyDown(event) {
+      if (event.code === el.keyCode) {
+        key.classList.add('active');
+        textarea.value += `${el.keyName}`;
+
+      }
+    });
+    document.addEventListener('keyup', function keyUp(event) {
+      if (event.code === el.keyCode) {
+        key.classList.remove('active');
+      }
+    });
+
+    key.addEventListener('mousedown', () => {
+      key.classList.add('active');
+      textarea.value += `${el.keyName}`;
+
+    }
+    );
+    key.addEventListener('mouseup', () => {
+      key.classList.remove('active');
+    }
+    );
+  }
+  );
 
 }());
